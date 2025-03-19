52f778e47f94dac975921c9c9555d58dd98613f1
Uzair Imtiaz <mtu880@gmail.com>
2025-03-19 23:19:43 +0500

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://your-http-api.com",
+        target: "http://static.112.170.75.5.clients.your-server.de:3003",
         changeOrigin: true,
         secure: false,
       },
