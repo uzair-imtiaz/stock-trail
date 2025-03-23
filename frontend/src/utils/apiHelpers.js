5245360d57f1226351df979bd1987d9a81816da9
Muhammad Ali <2020cs614@student.uet.edu.pk>
2025-03-23 23:38:49 +0500

    cors-4

--- frontend/src/utils/apiHelpers.js
+++ frontend/src/utils/apiHelpers.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Access-Control-Allow-Credentials': 'https://stock-trail-uzair-imtiazs-projects.vercel.app'
+    'Access-Control-Allow-Credentials': 'true'
   },
 });
 
